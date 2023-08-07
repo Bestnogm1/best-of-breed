0f04cb337fb6745497b40fcb25d2b946ad8ce2b5
Bestnogm1 <koffiarielhessou@gmail.com>
2023-08-07 11:26:43 -0400

    Added index action to retrieve all articles from the database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,12 @@
 class ArticlesController < ApplicationController
+
+  def index
+    begin 
+      @articles = Article.all.order(published_at: :desc)
+      render json: @articles
+    rescue ActiveRecord::RecordNotFound
+      render json: @articles.errors
+    end
+  end
+  
 end
