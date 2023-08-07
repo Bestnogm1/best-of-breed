4e9f44b3866c92709e8d33c08538f1c50db87d6b
Bestnogm1 <koffiarielhessou@gmail.com>
2023-08-07 14:46:09 -0400

    disable automatic timestamp recording and added validation for the title, content, author andpublished_at also enforced maximum length of 40 characters for the title

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+    self.record_timestamps = false
+    validates :title, presence: true, length: { maximum: 40 }
+    validates :content, presence: true
+    validates :author, presence: true
+    validates :published_at, presence: true
 end
