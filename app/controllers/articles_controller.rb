e9f8b6b581ace9c0ab86cf5b73dc111f7aaadbd2
Bestnogm1 <koffiarielhessou@gmail.com>
2023-08-07 11:46:43 -0400

    added the create the article function with takes in the article_params witch returns the and sanitize the incoming parameters and if the article is not valid it will return an error and with a status of unprocessable_entity

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,23 @@ class ArticlesController < ApplicationController
       render json: @articles.errors
     end
   end
-  
+
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      render json: @article, status: :created
+    else
+      # render status: 422
+      render json: { errors: @article.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def article_params
+    return params.permit(:title,
+    :content,
+    :author,
+    :category,
+    :published_at)
+  end
 end
